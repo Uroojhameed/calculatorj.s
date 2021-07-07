64350a0481d58be3d47142d3ddf1d228a51179ed
Uroojhameed <81069857+Uroojhameed@users.noreply.github.com>
2021-07-07 20:19:19 +0500

    Add files via upload
    assignment is complete

---
+++ app.js
@@ -0,0 +1,18 @@
+function getNum(num) {
+    var rsl =document.getElementById("rsl");
+    rsl.value += num;
+}
+function clearRsl(){
+    var rsl = document.getElementById("rsl");
+    rsl.value = "";
+}
+function showresult(){
+    var rsl = document.getElementById("rsl");
+    rsl.value = eval (rsl.value);
+}
+
+
+
+
+
+
